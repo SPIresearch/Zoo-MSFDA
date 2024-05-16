4181868f303d760e57dc78b1ea51b3b95ecb13af
SPI LAB@PKU <108445313+SPIresearch@users.noreply.github.com>
2024-05-16 16:37:13 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,367 @@
-from stringprep import c22_specials
-import numpy as np
+import math
 import torch
+import warnings
+import numpy as np
 import torch.nn as nn
 from torch.autograd import Variable
-import math
 import torch.nn.functional as F
-import pdb
+from scipy.stats import gamma
+#from numba import njit
+from torch.utils.data import TensorDataset
+from torch.utils.data import DataLoader
+from torch.optim import SGD
+from __future__ import division
+
+#@njit
+def each_evidence(y_, f, fh, v, s, vh, N, D):
+    """
+    compute the maximum evidence for each class
+    """
+    epsilon = 1e-5
+    alpha = 1.0
+    beta = 1.0
+    lam = alpha / beta
+    tmp = (vh @ (f @ np.ascontiguousarray(y_)))
+    for _ in range(11):
+        # should converge after at most 10 steps
+        # typically converge after two or three steps
+        gamma = (s / (s + lam)).sum()
+        # A = v @ np.diag(alpha + beta * s) @ v.transpose() # no need to compute A
+        # A_inv = v @ np.diag(1.0 / (alpha + beta * s)) @ v.transpose() # no need to compute A_inv
+        m = v @ (tmp * beta / (alpha + beta * s))
+        alpha_de = (m * m).sum()
+        alpha = gamma / (alpha_de + epsilon)
+        beta_de = ((y_ - fh @ m) ** 2).sum()
+        beta = (N - gamma) / (beta_de + epsilon)
+        new_lam = alpha / beta
+        if np.abs(new_lam - lam) / lam < 0.01:
+            break
+        lam = new_lam
+    evidence = D / 2.0 * np.log(alpha) \
+               + N / 2.0 * np.log(beta) \
+               - 0.5 * np.sum(np.log(alpha + beta * s)) \
+               - beta / 2.0 * (beta_de + epsilon) \
+               - alpha / 2.0 * (alpha_de + epsilon) \
+               - N / 2.0 * np.log(2 * np.pi)
+    return evidence / N, alpha, beta, m
+
+
+# use pseudo data to compile the function
+# D = 20, N = 50
+f_tmp = np.random.randn(20, 50).astype(np.float64)
+each_evidence(np.random.randint(0, 2, 50).astype(np.float64), f_tmp, f_tmp.transpose(), np.eye(20, dtype=np.float64), np.ones(20, dtype=np.float64), np.eye(20, dtype=np.float64), 50, 20)
+
+
+#@njit
+def truncated_svd(x):
+    u, s, vh = np.linalg.svd(x.transpose() @ x)
+    s = np.sqrt(s)
+    u_times_sigma = x @ vh.transpose()
+    k = np.sum((s > 1e-10) * 1)  # rank of f
+    s = s.reshape(-1, 1)
+    s = s[:k]
+    vh = vh[:k]
+    u = u_times_sigma[:, :k] / s.reshape(1, -1)
+    return u, s, vh
+truncated_svd(np.random.randn(20, 10).astype(np.float64))
+
+
+class LogME(object):
+    def __init__(self, regression=False):
+        """
+            :param regression: whether regression
+        """
+        self.regression = regression
+        self.fitted = False
+        self.reset()
+
+    def reset(self):
+        self.num_dim = 0
+        self.alphas = []  # alpha for each class / dimension
+        self.betas = []  # beta for each class / dimension
+        # self.ms.shape --> [C, D]
+        self.ms = []  # m for each class / dimension
+
+    def _fit_icml(self, f: np.ndarray, y: np.ndarray):
+        """
+        LogME calculation proposed in the ICML 2021 paper
+        "LogME: Practical Assessment of Pre-trained Models for Transfer Learning"
+        at http://proceedings.mlr.press/v139/you21b.html
+        """
+        fh = f
+        f = f.transpose()
+        D, N = f.shape
+        v, s, vh = np.linalg.svd(f @ fh, full_matrices=True)
+
+        evidences = []
+        self.num_dim = y.shape[1] if self.regression else int(y.max() + 1)
+        for i in range(self.num_dim):
+            y_ = y[:, i] if self.regression else (y == i).astype(np.float64)
+            evidence, alpha, beta, m = each_evidence(y_, f, fh, v, s, vh, N, D)
+            evidences.append(evidence)
+            self.alphas.append(alpha)
+            self.betas.append(beta)
+            self.ms.append(m)
+        self.ms = np.stack(self.ms)
+        return np.mean(evidences)
+
+    def _fit_fixed_point(self, f: np.ndarray, y: np.ndarray):
+        """
+        LogME calculation proposed in the arxiv 2021 paper
+        "Ranking and Tuning Pre-trained Models: A New Paradigm of Exploiting Model Hubs"
+        at https://arxiv.org/abs/2110.10545
+        """
+        N, D = f.shape  # k = min(N, D)
+        if N > D: # direct SVD may be expensive
+            u, s, vh = truncated_svd(f)
+        else:
+            u, s, vh = np.linalg.svd(f, full_matrices=False)
+        # u.shape = N x k
+        # s.shape = k
+        # vh.shape = k x D
+        s = s.reshape(-1, 1)
+        sigma = (s ** 2)
+
+        evidences = []
+        self.num_dim = y.shape[1] if self.regression else int(y.max() + 1)
+        for i in range(self.num_dim):
+            y_ = y[:, i] if self.regression else (y == i).astype(np.float64)
+            y_ = y_.reshape(-1, 1)
+            x = u.T @ y_  # x has shape [k, 1], but actually x should have shape [N, 1]
+            x2 = x ** 2
+            res_x2 = (y_ ** 2).sum() - x2.sum()  # if k < N, we compute sum of xi for 0 singular values directly
+
+            alpha, beta = 1.0, 1.0
+            for _ in range(11):
+                t = alpha / beta
+                gamma = (sigma / (sigma + t)).sum()
+                m2 = (sigma * x2 / ((t + sigma) ** 2)).sum()
+                res2 = (x2 / ((1 + sigma / t) ** 2)).sum() + res_x2
+                alpha = gamma / (m2 + 1e-5)
+                beta = (N - gamma) / (res2 + 1e-5)
+                t_ = alpha / beta
+                evidence = D / 2.0 * np.log(alpha) \
+                           + N / 2.0 * np.log(beta) \
+                           - 0.5 * np.sum(np.log(alpha + beta * sigma)) \
+                           - beta / 2.0 * res2 \
+                           - alpha / 2.0 * m2 \
+                           - N / 2.0 * np.log(2 * np.pi)
+                evidence /= N
+                if abs(t_ - t) / t <= 1e-3:  # abs(t_ - t) <= 1e-5 or abs(1 / t_ - 1 / t) <= 1e-5:
+                    break
+            evidence = D / 2.0 * np.log(alpha) \
+                       + N / 2.0 * np.log(beta) \
+                       - 0.5 * np.sum(np.log(alpha + beta * sigma)) \
+                       - beta / 2.0 * res2 \
+                       - alpha / 2.0 * m2 \
+                       - N / 2.0 * np.log(2 * np.pi)
+            evidence /= N
+            m = 1.0 / (t + sigma) * s * x
+            m = (vh.T @ m).reshape(-1)
+            evidences.append(evidence)
+            self.alphas.append(alpha)
+            self.betas.append(beta)
+            self.ms.append(m)
+        self.ms = np.stack(self.ms)
+        return np.mean(evidences)
+
+    _fit = _fit_fixed_point
+
+    def fit(self, f: np.ndarray, y: np.ndarray):
+      
+        if self.fitted:
+            warnings.warn('re-fitting for new data. old parameters cleared.')
+            self.reset()
+        else:
+            self.fitted = True
+        f = f.astype(np.float64)
+        if self.regression:
+            y = y.astype(np.float64)
+            if len(y.shape) == 1:
+                y = y.reshape(-1, 1)
+        return self._fit(f, y)
+
+    def predict(self, f: np.ndarray):
+      
+        if not self.fitted:
+            raise RuntimeError("not fitted, please call fit first")
+        f = f.astype(np.float64)
+        logits = f @ self.ms.T
+        if self.regression:
+            return logits
+        return np.argmax(logits, axis=-1)
+    
+
+def LEEP(pseudo_source_label: np.ndarray, target_label: np.ndarray):
+    
+    
+    N, C_s = pseudo_source_label.shape
+    target_label = target_label.reshape(-1)
+    C_t = int(np.max(target_label) + 1)   
+    normalized_prob = pseudo_source_label / float(N) 
+    joint = np.zeros((C_t, C_s), dtype=float)  
+    for i in range(C_t):
+        this_class = normalized_prob[target_label == i]
+        row = np.sum(this_class, axis=0)
+        joint[i] = row
+    p_target_given_source = (joint / joint.sum(axis=0, keepdims=True)).T  # P(y | z)
+
+    empirical_prediction = pseudo_source_label @ p_target_given_source
+    empirical_prob = np.array([predict[label] for predict, label in zip(empirical_prediction, target_label)])
+    leep_score = np.mean(np.log(empirical_prob))
+    return leep_score
+
+
+
+
+def SC_cal(pred_label: np.ndarray, struc_label: np.ndarray):
+   
+    
+    C_s = int(np.max(struc_label) + 1)  
+    C_p = int(np.max(pred_label) + 1)  
+    N = len(pred_label)
+    joint = np.zeros((C_s, C_p), dtype=float)  
+    for p, s in zip(pred_label, struc_label):
+        s = int(s)
+        p = int(p)
+        joint[s, p] += 1.0 / N
+    p_z = joint.sum(axis=0, keepdims=True) 
+    p_target_given_source = (joint / p_z).T  
+
+    mask = p_z.reshape(-1) != 0 
+    p_target_given_source = p_target_given_source[mask] + 1e-20 
+    entropy_y_given_z = np.sum(- p_target_given_source * np.log(p_target_given_source), axis=1, keepdims=True) 
+    conditional_entropy = np.sum(entropy_y_given_z * p_z.reshape((-1, 1))[mask])
+
+    
+    return - conditional_entropy
+
+
+
+
+class ANet(nn.Module):
+    def __init__(self, in_feature):
+        super(ANet, self).__init__()
+        self.layer = nn.Linear(in_feature, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.layer(x)
+        x = self.sigmoid(x)
+        return x
+
+
+def calculate_a_distance(source_feature: torch.Tensor, target_feature: torch.Tensor,
+              device='cuda:0', progress=True, training_epochs=10):
+
+    source_label = torch.ones((source_feature.shape[0], 1))
+    target_label = torch.zeros((target_feature.shape[0], 1))
+    feature = torch.cat([source_feature, target_feature], dim=0)
+    label = torch.cat([source_label, target_label], dim=0)
+
+    dataset = TensorDataset(feature, label)
+    length = len(dataset)
+    train_size = int(0.8 * length)
+    val_size = length - train_size
+    train_set, val_set = torch.utils.data.random_split(dataset, [train_size, val_size])
+    train_loader = DataLoader(train_set, batch_size=64, shuffle=True)
+    val_loader = DataLoader(val_set, batch_size=64, shuffle=False)
+
+    anet = ANet(feature.shape[1]).to(device)
+    for k, v in anet.named_parameters():
+        v.requires_grad = True
+    optimizer = SGD(anet.parameters(), lr=0.01)
+    a_distance = 2.0
+    for epoch in range(training_epochs):
+        anet.train()
+        for (x, label) in train_loader:
+            x = x.to(device)
+          
+            label = label.to(device)
+            anet.zero_grad()
+            y = anet(x)
+            loss = F.binary_cross_entropy(y, label)
+            
+            loss.backward()
+            optimizer.step()
+
+        anet.eval()
+     
+        acc=0
+        i=0
+        with torch.no_grad():
+            for (x, label) in val_loader:
+                x = x.to(device)
+                label = label.to(device)
+                y = anet(x)
+                pred=(y > 0.5)+0
+                acc += torch.sum(pred==label+0).float()
+                i+=x.shape[0]
+               
+        acc=acc/i
+        error = 1 - acc 
+        a_distance = 2 * (1 - 2 * error)
+      
+
+    return a_distance
+
+
+def guassian_kernel(source, target, kernel_mul=2.0, kernel_num=5, fix_sigma=None):
+    
+    n_samples = int(source.size()[0])+int(target.size()[0])
+    total = torch.cat([source, target], dim=0)
+ 
+    total0 = total.unsqueeze(0).expand(int(total.size(0)), int(total.size(0)), int(total.size(1)))
+ 
+    total1 = total.unsqueeze(1).expand(int(total.size(0)), int(total.size(0)), int(total.size(1)))
+  
+    L2_distance = ((total0-total1)**2).sum(2) 
+    
+    if fix_sigma:
+        bandwidth = fix_sigma
+    else:
+        bandwidth = torch.sum(L2_distance.data) / (n_samples**2-n_samples)
+ 
+    bandwidth /= kernel_mul ** (kernel_num // 2)
+    bandwidth_list = [bandwidth * (kernel_mul**i) for i in range(kernel_num)]
+  
+    kernel_val = [torch.exp(-L2_distance / bandwidth_temp) for bandwidth_temp in bandwidth_list]
+   
+    return sum(kernel_val)
+
+def mmd_rbf(source, target, kernel_mul=2.0, kernel_num=5, fix_sigma=None):
+   
+    n=(source.size()[0])
+    m=(target.size()[0])
+    batch_size = int(source.size()[0])
+    kernels = guassian_kernel(source, target,
+        kernel_mul=kernel_mul, kernel_num=kernel_num, fix_sigma=fix_sigma)
+    
+    XX = kernels[:batch_size, :batch_size]
+    YY = kernels[batch_size:, batch_size:]
+    XY = kernels[:batch_size, batch_size:]
+    YX = kernels[batch_size:, :batch_size]
+   
+    loss = torch.mean(XX + YY - XY -YX)
+  
+    return loss
+def mmd_loss(d_list,cen):
+    """
+    Args:
+        inputs: feature matrix with shape (batch_size, feat_dim)
+        targets: ground truth labels with shape (num_classes)
+    """
+   
+    mmds=[]
+    n = len(d_list)
+    for i in range(n):
+        f1=d_list[i]
+        f2=cen
+        mmds.append(mmd_rbf(f1,f2))
+    loss=sum(mmds)
+
+   
+    return loss
 
 class Loss_Record(nn.Module):
     """Cross entropy loss with label smoothing regularizer.
@@ -54,3 +410,90 @@ def get_source_model_path(pth_save_dir,dset,model_name,source):
     model_name='_'+model_name
     path=pth_save_dir+model_name+'/'+dset+'/'+source+'/'
     return path
+
+
+
+def rbf_dot(pattern1, pattern2, deg):
+	size1 = pattern1.shape
+	size2 = pattern2.shape
+
+	G = np.sum(pattern1*pattern1, 1).reshape(size1[0],1)
+	H = np.sum(pattern2*pattern2, 1).reshape(size2[0],1)
+
+	Q = np.tile(G, (1, size2[0]))
+	R = np.tile(H.T, (size1[0], 1))
+
+	H = Q + R - 2* np.dot(pattern1, pattern2.T)
+
+	H = np.exp(-H/2/(deg**2))
+
+	return H
+
+
+def hsic_gam(X, Y, alph = 0.5):
+	"""
+	X, Y are numpy vectors with row - sample, col - dim
+	alph is the significance level
+	auto choose median to be the kernel width
+	"""
+	n = X.shape[0]
+
+	# ----- width of X -----
+	Xmed = X
+
+	G = np.sum(Xmed*Xmed, 1).reshape(n,1)
+	Q = np.tile(G, (1, n) )
+	R = np.tile(G.T, (n, 1) )
+
+	dists = Q + R - 2* np.dot(Xmed, Xmed.T)
+	dists = dists - np.tril(dists)
+	dists = dists.reshape(n**2, 1)
+
+	width_x = np.sqrt( 0.5 * np.median(dists[dists>0]) )
+	# ----- -----
+
+	# ----- width of X -----
+	Ymed = Y
+
+	G = np.sum(Ymed*Ymed, 1).reshape(n,1)
+	Q = np.tile(G, (1, n) )
+	R = np.tile(G.T, (n, 1) )
+
+	dists = Q + R - 2* np.dot(Ymed, Ymed.T)
+	dists = dists - np.tril(dists)
+	dists = dists.reshape(n**2, 1)
+
+	width_y = np.sqrt( 0.5 * np.median(dists[dists>0]) )
+	# ----- -----
+
+	bone = np.ones((n, 1), dtype = float)
+	H = np.identity(n) - np.ones((n,n), dtype = float) / n
+
+	K = rbf_dot(X, X, width_x)
+	L = rbf_dot(Y, Y, width_y)
+
+	Kc = np.dot(np.dot(H, K), H)
+	Lc = np.dot(np.dot(H, L), H)
+
+	testStat = np.sum(Kc.T * Lc) / n
+
+	varHSIC = (Kc * Lc / 6)**2
+
+	varHSIC = ( np.sum(varHSIC) - np.trace(varHSIC) ) / n / (n-1)
+
+	varHSIC = varHSIC * 72 * (n-4) * (n-5) / n / (n-1) / (n-2) / (n-3)
+
+	K = K - np.diag(np.diag(K))
+	L = L - np.diag(np.diag(L))
+
+	muX = np.dot(np.dot(bone.T, K), bone) / n / (n-1)
+	muY = np.dot(np.dot(bone.T, L), bone) / n / (n-1)
+
+	mHSIC = (1 + muX * muY - muX - muY) / n
+
+	al = mHSIC**2 / varHSIC
+	bet = varHSIC*n / mHSIC
+
+	thresh = gamma.ppf(1-alph, al, scale=bet)[0][0]
+
+	return (testStat, thresh)
